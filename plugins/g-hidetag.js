21bc004825a04f0f9c015a8baf139aff3bde701c
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-18 05:58:54 +0700

    Update g-hidetag.js

--- plugins/g-hidetag.js
+++ plugins/g-hidetag.js
@@ -1,20 +1,30 @@
 let { generateWAMessageFromContent } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, text, participants }) => {
-  let users = participants.map(u => conn.decodeJid(u.id))
+  let users = participants.map(u => u.id)
   let q = m.quoted ? m.quoted : m
   let c = m.quoted ? m.quoted : m.msg
-  const msg = conn.cMod(m.chat,
-    generateWAMessageFromContent(m.chat, {
-      [c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
-        text: c || ''
+  let msg = await conn.cMod(
+    m.chat,
+    await generateWAMessageFromContent(
+      m.chat,
+      {
+        [c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
+          text: await c || ''
+        },
+        mentions: await users
+      }, {
+        quoted: null,
+        userJid: conn.user.id
       }
-    }, {
-      quoted: m,
-      userJid: conn.user.id
-    }),
-    text || q.text, conn.user.jid, { mentions: users }
+    ),
+    text || q.text, conn.user.jid,
+    {
+      mentions: await users
+    }
   )
-  await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+  await conn.relayMessage(m.chat, msg.message, {
+    messageId: msg.key.id
+  })
 }
 handler.help = ['hidetag', 'pengumuman'].map(v => v + ' [teks]')
 handler.tags = ['group']
