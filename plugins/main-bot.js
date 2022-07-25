c15eaa257ffc4ecf7dc2da76896c61fdeff7c2a7
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-25 15:41:29 +0700

    Update main-bot.js

--- plugins/main-bot.js
+++ plugins/main-bot.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/70f95bcad271d303bf982.png', m, { packname: "ᵈʳᴋᴏᴋᴏ⁻ᴹᴰ×፝֟͜×", author: "@ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/70f95bcad271d303bf982.png', m, { packname: "KokoMD", author: "@kokopangeran" })
 }
 
-handler.customPrefix = /^(woi|Cok|ngab|tod|bang|hai|hi|hii)$/i
+handler.customPrefix = /^(bot|woi|Cok|ngab|tod|bang|hai|hi|hii)$/i
 handler.command = new RegExp
 
 module.exports = handler
