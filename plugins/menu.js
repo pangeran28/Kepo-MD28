feee80fb28ef68140bd586ffcdd3cda2dfd4ffaf
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-25 14:55:17 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -5,7 +5,7 @@ let fetch = require('node-fetch')
 let moment = require('moment-timezone')
 const defaultMenu = {
   before: `
-┌─〔 ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ-MD×፝֟͜× 〕
+┌─〔 ᵈʳᴋ ᴏ ᴋ ᴏ ⁻ ᴹ ᴰ×፝֟͜× 〕
 ├ *%ucapan %name*
 │
 ├ Tersisa *%limit Limit*
