6319d400370c005bd1107d08ddb32b89929c00c3
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-26 23:11:54 +0700

    Update d-tiktok.js

--- plugins/d-tiktok.js
+++ plugins/d-tiktok.js
@@ -1,15 +1,13 @@
-let axios = require('axios');
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
-    if (res.status != 200) throw res.message;
-    if (!res) throw res.message;
-    conn.sendButtonVid(m.chat, res.video, `*Judul:* ${res.title}\n${res.author ? `*Pembuat Video:* ${res.author}` : '\n' }`.trim(), 'Cara simpan digalery:\n1. Download dulu videonya\n2. Buka terus klik titik 3 pojok kanan atas\n3. lalu klik simpan!', 'menu', usedPrefix + 'menu', m)
+const hxz = require("hxz-api")
+let handler = async(m,{text, conn}) => {
+
+m.reply(wait)
+let p = await  hxz.ttdownloader(text)
+const { nowm, wm, audio } = p
+conn.sendFile(m.chat, nowm, null, '*DONE YA SAYANG✅*\n\n_*Jangan Lupa Follow IG OwnerKu Ya Tante:*_\nhttps://instagram.com/kokopangeran_', m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tik(tok)?(dl)?)$/i
-handler.premium = true
-handler.limit = 1
+handler.command = /^(tiktok|tiktoknowm)$/i
 
 module.exports = handler
