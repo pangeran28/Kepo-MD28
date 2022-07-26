51fca56ece3c9a92f61d7306ca834b1b7b1012b9
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-26 23:36:34 +0700

    Update d-tiktok.js

--- plugins/d-tiktok.js
+++ plugins/d-tiktok.js
@@ -1,14 +1,16 @@
 const hxz = require("hxz-api")
-let handler = async(m,{text, conn}) => {
-
-m.reply(wait)
-let p = await  hxz.ttdownloader(text)
+let handler = async(m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `*Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
+if (!args[0].match(/tiktok/gi)) throw `*Link salah! Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
+let p = await  hxz.ttdownloader(args[0])
 const { nowm, wm, audio } = p
-conn.sendFile(m.chat, nowm, null, '*DONE YA SAYANG✅*\n\n_*Jangan Lupa Follow IG OwnerKu Ya Tante:*_\nhttps://instagram.com/kokopangeran_', m)
+// made by aine
+ conn.sendFile(m.chat, nowm, 'tiktok.mp4', `*${global.wm}*`, m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktok|tiktoknowm)$/i
+handler.command = /^(tiktok|tiktokdl)$/i
 handler.limit = true
-handler.premium = true
+handler.group = false
+
 module.exports = handler
