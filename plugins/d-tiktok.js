b3338a0abb778e2ce9acb1823c901f11d8489fda
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-27 00:15:14 +0700

    Update d-tiktok.js

--- plugins/d-tiktok.js
+++ plugins/d-tiktok.js
@@ -1,16 +1,21 @@
-const hxz = require("hxz-api")
-let handler = async(m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `*Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
-if (!args[0].match(/tiktok/gi)) throw `*Link salah! Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
-let p = await  hxz.ttdownloader(args[0])
-const { nowm, wm, audio } = p
-// made by aine
- conn.sendFile(m.chat, nowm, 'tiktok.mp4', `*${global.wm}*`, m)
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+
+  if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/yqyjPX/`
+  if (!args[0].match(/tiktok/gi)) throw `url salah`
+
+  let res = await fetch(global.API('hardianto', '/api/download/tiktok', { url: args[0] }, 'apikey'))
+  if (!res.ok) throw await `${res.status} ${res.statusText}`
+  let json = await res.json()
+  // if (!json.status) throw json
+  await m.reply(global.wait)
+  await conn.sendFile(m.chat, json.nowm, '', `${json.caption}\n\nBOTSTYLE`, m)
+
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktok|tiktokdl)$/i
+handler.command = /^(tiktok)$/i
+handler.premium = true
 handler.limit = true
-handler.group = false
 
 module.exports = handler
