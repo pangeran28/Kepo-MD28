76dc644b3088e45ccc763c45a09358be67a294e5
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-27 02:12:59 +0700

    Update beban.js

--- plugins/beban.js
+++ plugins/beban.js
@@ -1,6 +1,6 @@
 let handler = async m => {
 
-let Beban = `Sampai Jumpa Beban`
+let beban = `Sampai Jumpa Beban`
 m.reply(`
 _*Akhirnya Beban Keluarga Out Juga, Tolong Bilangin Ke Yang Tadi Keluar Nitip Kondom Buat Semua Orang Group Sini💦*_
 `.trim()) // Tambah sendiri kalo mau
