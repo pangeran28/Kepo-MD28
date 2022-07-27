2e84f269a7137a765152168e9cecb80f6d53cf78
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-27 10:46:02 +0700

    Update beban.js

--- plugins/beban.js
+++ plugins/beban.js
@@ -5,6 +5,7 @@ m.reply(`
 _*Akhirnya Beban Keluarga Out Juga, Tolong Bilangin Ke Yang Tadi Keluar Nitip Kondom Buat Semua Orang Group Sini💦*_
 `.trim()) // Tambah sendiri kalo mau
 }
-handler.command = /^(beban)$/i
+handler.customPrefix = /beban|Beban/i
+handler.command = new RegExp
 
 module.exports = handler
