892366c05994f477daaa5a5476c28d959f10395f
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-29 02:54:36 +0700

    Update nsfw-all3.js

--- plugins/nsfw-all3.js
+++ plugins/nsfw-all3.js
@@ -5,5 +5,7 @@ let nyenye = `https://api.lolhuman.xyz/api/random2/${command}?apikey=${lolkey}`
 handler.help = ['lewd', 'eron', 'solo', 'anal', 'keta', 'tits', 'kuni', 'solog', 'erok', 'feetg', 'lewdk', 'erofeet', 'holoero', 'classic', 'erokemo', 'futanari', 'eroyuri', 'yaoi']
 handler.tags = ['nsfw']
 handler.command = /^(lewd|eron|solo|anal|keta|tits|kuni|solog|erok|feetg|lewdk|erofeet|holoero|classic|erokemo|futanari|eroyuri|yaoi)$/i
+handler.premium = true
+handler.limit = true
 //buatan hyzer, jgn hapus nama gw atuh 😊
 module.exports = handler
