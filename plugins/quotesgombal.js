75c6f7590081f9a485b2349a20cefc9d6dc13f69
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-29 04:58:26 +0700

    Update quotesgombal.js

--- plugins/quotesgombal.js
+++ plugins/quotesgombal.js
@@ -1,42 +1,18 @@
-let handler = async (m, { conn } ) => {
-let anu =`─────〔 *Gombal* 〕─────\n\n${pickRandom(global.gombal)}`
-const ftroli = {
-    key : {
-    remoteJid: '6283136505591-1614953337@g.us',
-    participant : '0@s.whatsapp.net'
-    },
-    message: {
-    orderMessage: {
-    itemCount : 2022,
-    status: 1,
-    surface : 1,
-    message: 'Random Gombal', 
-    orderTitle: `Wabot`,
-    thumbnail: 'https://telegra.ph/file/be35f3f279c9af2d607e8.jpg', 
-    sellerJid: '0@s.whatsapp.net' 
-    }
-    }
-    }
-conn.sendBut(m.chat, anu, wm, 'Gombal Sayang', '.gombal', ftroli) 
+let fs = require('fs')
+let handler = async (m, { conn }) => {
+let anu =`
+─────〔 *GOMBAL* 〕─────
+${pickRandom(global.gombal)}
+`
+conn.reply(m.chat, anu, m) 
 }
 handler.help = ['gombal']
 handler.tags = ['quotes']
 handler.command = /^(gombal)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-
 module.exports = handler
 
 function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
+    return list[Math.floor(list.length * Math.random())]
 }
 
 global.gombal = [
