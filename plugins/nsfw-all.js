69bea7896baf5ebe0ddc7de65618aa8f08ebac78
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-29 02:09:56 +0700

    Create nsfw-all.js

---
+++ plugins/nsfw-all.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, command }) => {
+let nyenye = `https://zenzapis.xyz/api/morensfw/${command}?apikey=${zenzkey}`
+    conn.sendButtonImg(m.chat, nyenye, 'Sudah Crot Syg', wm, 'Crot Lagi Yuk', `.${command}`, m) 
+}
+handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'cum', 'ero', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'jahy', 'maid', 'manga', 'neko', 'orgy', 'panties', 'pussy','sfwneko', 'tentacles', 'thighs', 'yuri']
+handler.tags = ['nsfw']
+handler.command = /^(ahegao|ass|bdsm|blowjob|cum|ero|femdom|foot|gangbang|glasses|hentai|jahy|maid|manga|neko|orgy|panties|pussy|sfwneko|tentacles|thighs|yuri)$/i
+//buatan hyzer, jgn hapus atuh 😊
+module.exports = handler
