7b9e9a4ac1243df60cc7e60fa949b51bfec4d635
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-29 02:12:21 +0700

    Create nsfw-all2.js

---
+++ plugins/nsfw-all2.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, command }) => {
+let nyenye = `https://api.lolhuman.xyz/api/random/nsfw/${command}?apikey=${lolkey}`
+    conn.sendButtonImg(m.chat, nyenye, 'Sudah Crot Sayang', wm, 'Crot Lagi Yuk', `.${command}`, m) 
+}
+handler.help = ['hollolewd', 'sideoppai', 'animefeets', 'animebooty', 'animethighss', 'animearmpits', 'lewdanimegirls', 'biganimetiddies']
+handler.tags = ['nsfw']
+handler.command = /^(hollolewd|sideoppai|animefeets|animebooty|animethighss|animearmpits|lewdanimegirls|biganimetiddies)$/i
+//buatan hyzer, jgn hapus atuh 😊
+module.exports = handler
