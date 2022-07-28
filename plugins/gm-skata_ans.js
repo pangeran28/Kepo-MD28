baa7648ec5768223dbb563d0426f172e1f098060
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-28 13:08:40 +0700

    Update gm-skata_ans.js

--- plugins/gm-skata_ans.js
+++ plugins/gm-skata_ans.js
@@ -1,8 +1,4 @@
-const axios = require('axios')
-const delay = ms => new Promise(res => setTimeout(res, ms))
-const { newMessagesDB } = require("@adiwajshing/baileys")
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
+const skata = require('../lib/sambung-kata')
 let handler = m => m
 
 handler.before = async function (m) {
@@ -44,7 +40,7 @@ handler.before = async function (m) {
 		room.new = false
 		room.killer = false
 		room.kata = _kata
-		room.chat = await this.reply(m.chat, `giliran @${room.curr.split(`@`)[0]} untuk menjawab.\nMulai : *${(_kata).toUpperCase()}*\n*${room.filter(_kata).toUpperCase()}... ?*\n\n*balas pesan ini untuk menjawab!*\nXP terkumpul: ${room.win_point}\nTersisa: \n${readaMore + room.player.map((v, i) => i + 1 + '. ' + users[v].name).join('\n')}`, m)
+		room.chat = await this.reply(m.chat, `Saatnya @${room.curr.split(`@`)[0]}\nMulai : *${(_kata).toUpperCase()}*\n*${room.filter(_kata).toUpperCase()}... ?*\n*Reply untuk menjawab!*\n"nyerah" untuk menyerah\nXP terkumpul: ${room.win_point}\nTersisa: \n${this.readmore + room.player.map((v, i) => i + 1 + '. ' + users[v].name).join('\n')}`, 0)
 	}
 	if (room.diam) {
 		if (!/nextkata/i.test(m.text)) return !0
@@ -52,39 +48,43 @@ handler.before = async function (m) {
 		room.waktu = setTimeout(() => {
 			lose_skata = mmr('lose', room.curr)
 			win_skata = (room.killer ? mmr('win', room.killer) : null)
-			this.reply(m.chat, `\n *waktu untuk menjawab soal telah habis*\n@${room.curr.split`@`[0]} telah tereliminasi -${lose_skata} MMR${room.killer ? `\n@${room.killer.split`@`[0]} +${win_skata} MMR` : ''}`, room.chat)
-			room.eliminated.push(room.curr)
-			if (room.killer) {
-				users[room.killer].skata += win_skata
-				users[room.curr].skata -= lose_skata
-			}
-			let index = member.indexOf(room.curr)
-			member.splice(index, 1)
-			if (index == member.length) room.curr = member[0]
-			else room.curr = member[index]
-			if (member.length == 1 && room.status == 'play') {
-				this.sendButton(m.chat, `selamat buat @${member[0].split`@`[0]} karena berhasil bertahan!\n\n *+${room.win_point}XP*`, wm, 'sambung kata', '.skata', room.chat)
-				users[member[0]].exp += room.win_point
-				delete this.skata[id]
-				return !0
-			} else {
-				room.diam = true
-				room.new = true
-				who = room.curr
-				this.emit('chat-update', {
-					jid: who,
-					hasNewMessage: true,
-					messages: newMessagesDB([this.cMod(m.chat, m, 'nextkata', who)])
-				})
-			}
+			this.reply(m.chat, `Waktu jawab habis\n@${room.curr.split`@`[0]} tereliminasi -${lose_skata} MMR${room.killer ? `\n@${room.killer.split`@`[0]} +${win_skata} MMR` : ''}`, room.chat).then(_ => {
+				room.eliminated.push(room.curr)
+				if (room.killer) {
+
+					users[room.killer].skata += win_skata
+					users[room.curr].skata -= lose_skata
+				}
+				let index = member.indexOf(room.curr)
+				member.splice(index, 1)
+				if (index == member.length) room.curr = member[0]
+				else room.curr = member[index]
+				if (member.length == 1 && room.status == 'play') {
+					this.sendButton(m.chat, `@${member[0].split`@`[0]} Berhasil bertahan`, `+${room.win_point}XP`, 2, ['Sambung Kata', '.skata', 'Top Player', '.topskata'], room.chat).then(_ => {
+						users[member[0]].exp += room.win_point
+						delete this.skata[id]
+						return !0
+					})
+
+
+				} else {
+					room.diam = true
+					room.new = true
+					who = room.curr
+					conn.preSudo('nextkata', who, m).then(async _ => {
+						this.ev.emit('messages.upsert', _)
+					})
+				}
+			})
 		}, 30000)
 	}
+
 	if (room.curr == m.sender) {
 		if (/nyerah/i.test(m.text)) {
 			lose_skata = mmr('lose', room.curr)
 			win_skata = (room.killer ? mmr('win', room.killer) : null)
 			clearTimeout(room.waktu)
-			this.reply(m.chat, `@${room.curr.split`@`[0]} telah tereliminasi -${lose_skata} MMR${room.killer ? `\n@${room.killer.split`@`[0]} +${win_skata} MMR` : ''}`, room.chat)
+			this.reply(m.chat, `@${room.curr.split`@`[0]} tereliminasi -${lose_skata} MMR${room.killer ? `\n@${room.killer.split`@`[0]} +${win_skata} MMR` : ''}`, room.chat)
 			room.eliminated.push(room.curr)
 			if (room.killer) {
 				users[room.killer].skata += win_skata
@@ -95,7 +95,7 @@ handler.before = async function (m) {
 			if (index == (member.length)) room.curr = member[0]
 			else room.curr = member[index]
 			if (member.length == 1 && room.status == 'play') {
-				this.sendButton(m.chat, `selamat buat @${member[0].split`@`[0]} karena berhasil bertahan!\n\n *+${room.win_point}XP*\n*+${win_skata} MMR*`, wm, 'sambung kata', '.skata', room.chat)
+				await this.sendButton(m.chat, `@${member[0].split`@`[0]} Berhasil bertahan`, `+${room.win_point}XP`, 2, ['Sambung Kata', '.skata', 'Top Player', '.topskata'], room.chat, { contextInfo: { mentionedJid: member } })
 				users[member[0]].skata += win_skata
 				users[member[0]].exp += room.win_point
 				delete this.skata[id]
@@ -104,18 +104,13 @@ handler.before = async function (m) {
 			room.new = true
 			room.diam = true
 			who = room.curr
-			this.emit('chat-update', {
-				jid: who,
-				hasNewMessage: true,
-				messages: newMessagesDB([this.cMod(m.chat, m, 'nextkata', who)])
-			})
+			let msg = await conn.preSudo('nextkata', who, m)
+			this.ev.emit('messages.upsert', msg)
 		}
 		if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/(Mulai|Tersisa) ?:/i.test(m.quoted.text)) return !0
 		if (m.quoted.id == room.chat.id) {
 			let answerF = (m.text.toLowerCase().split` `[0]).trim().replace(/[^a-z]/gi, '')
-			let res = await axios.get(API('males', '/ceksambungkata', { kata: m.text.toLowerCase().split(` `)[0] })).catch(err => { return !0 })
-			// if (!res.ok) throw `Error: ` + res.statusText;
-			let checkF = await res.data
+			let checkF = await skata.cKata(m.text.toLowerCase().split` `[0])
 			if (!answerF.startsWith(room.filter(room.kata))) {
 				return m.reply(`👎🏻 *Salah!*\nJawaban harus dimulai dari kata *${room.filter(room.kata)}*`)
 			} else if (!checkF.status) {
@@ -133,40 +128,37 @@ handler.before = async function (m) {
 			if (waktunya + 1 >= member.length) room.curr = member[0]
 			room.basi.push(answerF)
 			room.win_point += 200
-			room.chat = await this.reply(m.chat, `*👍 +${bonus}XP*\nGiliran @${room.curr.split`@`[0]}\n*${room.filter(answerF).toUpperCase()}... ?*\n*balas pesan ini untuk menjawab!*\nketik *nyerah* untuk menyerah\nXP terkumpul: ${room.win_point}\nTersisa: \n${readMore + room.player.map((v, i) => i + 1 + '. ' + users[v].name).join('\n')}`, m)
+			room.chat = await this.reply(m.chat, `👍+${bonus}XP\nGiliran @${room.curr.split`@`[0]}\n*${room.filter(answerF).toUpperCase()}... ?*\n*Reply untuk menjawab!*\n"nyerah" untuk menyerah\nXP terkumpul: ${room.win_point}\nTersisa: \n${this.readmore + room.player.map((v, i) => i + 1 + '. ' + users[v].name).join('\n')}`, m)
 			room.diam = true
 			room.kata = answerF
 			who = room.curr
-			this.emit('chat-update', {
-				jid: who,
-				hasNewMessage: true,
-				messages: newMessagesDB([this.cMod(m.chat, m, 'nextkata', who)])
-			})
+			let msg = await conn.preSudo('nextkata', who, m)
+			this.ev.emit('messages.upsert', msg)
 			return !0
 		}
 	} else if (room.curr !== m.sender) {
 		if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/(Mulai|Tersisa) ?:/i.test(m.quoted.text)) return !0
 		if (m.quoted.id == room.chat.id) {
-			if (room.eliminated.includes(m.sender)) m.reply(`\n   _Hei, kamu sudah tereliminasi, tunggu hingga game ini selesai_\n   *Nice Try, next game*`)
+			if (room.eliminated.includes(m.sender)) m.reply(`_Hei, kamu sudah tereliminasi, tunggu hingga game ini selesai_\n*Nice Try, next game*`)
 			else if (room.player.includes(m.sender)) {
-				m.reply(`\n   _Bukan giliranmu.._\n`)
-			} else m.reply(`\n   _*Kamu tidak dapat menjawab soal itu*_\n   Karena kamu tidak bergabung dalam game ini\n\n   Tunggu hingga game ini berakhir, kemudian ikutlah bermain!`)
-		} else m.reply(`\n   Soal itu sudah lewat\n`)
+				m.reply(`_Bukan giliranmu.._`)
+			} else m.reply(`_*Kamu tidak dapat menjawab soal itu*_\nKarena kamu tidak bergabung dalam game ini\n\nTunggu hingga game ini berakhir, kemudian ikutlah bermain!`)
+		} else m.reply(`Soal itu sudah lewat`)
 	}
 	return !0
 }
-
 module.exports = handler
 
 async function genKata() {
-	let res = await axios.get(global.API('males', '/sambungkata')).catch(err => { return !0 })
-	let result = res.data.kata
+	let json = await skata.kata()
+	let result = json.kata
 	while (result.length < 3) {
-		res = await axios.get(global.API('males', '/sambungkata')).catch(err => { return !0 })
-		result = res.data.kata
+		json = await skata.kata()
+		result = json.kata
 	}
 	return result
 }
+
 
 function rwd(min, max) {
 	min = Math.ceil(min)
