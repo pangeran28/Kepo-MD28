fb7f7414b0fe9ec91964c45fe15fa452f3ecd7e1
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-29 23:53:39 +0700

    Update m-steel.js

--- plugins/m-steel.js
+++ plugins/m-steel.js
@@ -29,7 +29,7 @@ let handler = async (m, {
 	}
 }
 handler.help = ['steel [text]']
-handler.tags = ['maker', 'textpro']
+handler.tags = ['textpro']
 handler.command = /^steel$/i
 handler.limit = true
 
