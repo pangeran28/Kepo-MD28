a41d9e3bdeca0d0fa057c203f02c6d2c114d3c13
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-29 23:45:19 +0700

    Update m-glitter.js

--- plugins/m-glitter.js
+++ plugins/m-glitter.js
@@ -29,7 +29,7 @@ let handler = async (m, {
 	}
 }
 handler.help = ['glitter [text]']
-handler.tags = ['maker', 'textpro']
+handler.tags = ['textpro']
 handler.command = /^glitter$/i
 handler.limit = true
 
