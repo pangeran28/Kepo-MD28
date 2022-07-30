86def5d8fa80b295633e6257e23bb03135ae7793
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-30 14:16:22 +0700

    Update and rename nsfw-hentai.js to nsfw-nhentai.js

--- plugins/nsfw-hentai.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random2/hentai', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.hentai', m)
-}
-handler.help = ['hentai']
-handler.tags = ['nsfw']
-handler.command = /^(hentai)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
