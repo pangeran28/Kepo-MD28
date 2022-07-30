994c5c2a444ab07166c4f800aca8b06cc9e5a996
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-30 19:14:10 +0700

    Update rpg-mining.js

--- plugins/rpg-mining.js
+++ plugins/rpg-mining.js
@@ -1,64 +1,41 @@
 let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender]
-    let _md = `${Math.floor(Math.random() * 3500)}`.trim()
-    let md = (_md * 1)
     let __timers = (new Date - user.lastmining)
-    let _timers = (60000 - __timers)
+    let _timers = (300000 - __timers)
     let timers = clockString(_timers) 
-    if (new Date - user.lastmining > 60000) {
-        conn.reply(m.chat, `Anda mendapatkan *Rp.${md}* Money`, m)
-        global.db.data.users[m.sender].money += md * 1
-        global.db.data.users[m.sender].lastmining = new Date * 1
-    } else {
-        let buttons = button(`silahkan tunggu *${timers}* lagi untuk bisa mengclaim lagi`, user)
-        conn.sendMessage(m.chat, buttons, { quoted: m })
-    }
-}
+    let botol = global.wm
+    let minim = `${Math.floor(Math.random() * 3)}`.trim() 
+    let minam = `${Math.floor(Math.random() * 5)}`.trim() 
+    let minkm = `${Math.floor(Math.random() * 10)}`.trim() 
+
+   if ( user.pickaxe > 0 ) {
+    if (new Date - user.lastmining > 3000000) {
+
+      user.emas += minim * 1
+      user.iron += minam * 1
+      user.string += minkm * 1
+      user.lastmining = new Date * 1
+            
+    m.reply(`Kamu Menambang Di ${pickRandom(['⛰️Lembah', '⛰️Goa mletre', '🏞️Sungai Selandia', '⛰️Goa texas', '...'])}\n*⚒️Hasil Tambang:* 🪙Emas: *${minim}*\n⛓️Besi: *${minam}*\n🕸️String: *${minkm}*`)
+      } else conn.sendBut( m.chat, `Tunggu 🕓${timers} lagi, untuk menambang`, `${botol}`, `⋮☰ Back`, `.menu`, m)
+    } else conn.sendBut( m.chat, `Kamu Tidak Mempunyai *⛏️Pickaxe* untuk menambang\nBuat Lah Pickaxe menggunakan string kayu dan batu!`, `${wm}`, `Craft Pickaxe`, `.craft pickaxe`,m )
+  }
+
 handler.help = ['mining']
 handler.tags = ['rpg']
-handler.command = /^(mining)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = false
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-
+handler.command = /^mining/i
+handler.group = true
 module.exports = handler
 
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
 function clockString(ms) {
+  let h = Math.floor(ms / 3600000)
   let m = Math.floor(ms / 60000) % 60
   let s = Math.floor(ms / 1000) % 60
-  console.log({ms,m,s})
-  return [m, s].map(v => v.toString().padStart(2, 0) ).join(':')
+  console.log({ms,h,m,s})
+  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
 }
 
-function button(teks, user) {
-    const buttons = []
-    
-    let mining = new Date - user.lastmining > 60000
-    let claim = new Date - user.lastclaim > 86400000
-    let claim2 = new Date - user.lastclaim2 > 86400000
-    console.log({mining, claim, claim2})
-    
-    if (mining) buttons.push({buttonId: `.mining`, buttonText: {displayText: 'MINING'}, type: 1})
-    if (claim) buttons.push({buttonId: `.claim`, buttonText: {displayText: 'CLAIM'}, type: 1})
-    if (claim2) buttons.push({buttonId: `.claim2`, buttonText: {displayText: 'CLAIM2'}, type: 1})
-    if (buttons.length == 0) throw teks
-    
-    const buttonMessage = {
-            text: teks,
-            footer: wm,
-            buttons: buttons,
-            headerType: 1
-    }
-    
-    return buttonMessage
-}
+let wm = global.wm
