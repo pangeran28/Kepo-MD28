959961e9e0b19e73cd297854629a5d47d2c3b7f4
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:21:05 +0700

    Delete nsfw-tits.js

--- plugins/nsfw-tits.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random2/tits', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.tits', m)
-}
-handler.help = ['tits']
-handler.tags = ['nsfw']
-handler.command = /^(tits)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
