37c7e8af02cdd03e9aad6e2a0bd8604033714cf3
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-06 23:22:46 +0700

    Update _antiwame.js

--- plugins/_antiwame.js
+++ plugins/_antiwame.js
@@ -6,12 +6,12 @@ handler.before = async function (m, { isAdmin, isBotAdmin }) {
   let chat = global.db.data.chats[m.chat]
   let isWameLink = linkRegex.exec(m.text)
 
-  if (chat.antiLink && isWameLink && !isAdmin && !m.isBaileys && m.isGroup) {
-    let thisWame = `https://wa.me/${await this.groupInviteCode(m.chat)}`
+  if (chat.antiWame && isWameLink && !isAdmin && !m.isBaileys && m.isGroup) {
+    let thisWame = `https://wa.me/${await this.wameInviteCode(m.chat)}`
     if (m.text.includes(thisWame)) throw false // jika link grup itu sendiri gak dikick
       if (!isBotAdmin) m.reply(` *「 ANTI WA ME 」* ${isAdmin ? "Admin mah bebas ygy :v" : `\n\nlink group terdeteksi dan ${global.namabot} bukan admin jadi tidak bisa ngekick!`}`)
     if (isBotAdmin) {
-      m.reply(` *「 ANTI WA MR 」* \n\nLink Wa Me Terdeteksi, bye Kamu Akan Di Kick!!`.trim())
+      m.reply(` *「 ANTI WA ME 」* \n\nLink Wa Me Terdeteksi, bye Kamu Akan Di Kick!!`.trim())
       await this.delay(500)
       await this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
     }
