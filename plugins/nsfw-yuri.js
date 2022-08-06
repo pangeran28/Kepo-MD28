5ea0de1c6e9eb19df348528a3571eeae5b692689
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:36:05 +0700

    Delete nsfw-yuri.js

--- plugins/nsfw-yuri.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random2/yuri', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.yuri', m)
-}
-handler.help = ['yuri']
-handler.tags = ['nsfw']
-handler.command = /^(yuri)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
