f97c4753d9d4ae59fd10d6511617c804d4abd594
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:12:31 +0700

    Delete nsfw-neko.js

--- plugins/nsfw-neko.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/nsfw/neko', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.nsfwneko', m)
-}
-handler.help = ['nsfwneko']
-handler.tags = ['nsfw']
-handler.command = /^(nsfwneko)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
