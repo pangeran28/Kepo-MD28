7d985d488cfc49c6bf05f4d80f1025fc1f5e8cb2
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:23:05 +0700

    Delete nsfw-yaoi.js

--- plugins/nsfw-yaoi.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/nsfw/yaoi', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.yaoi', m)
-}
-handler.help = ['yaoi']
-handler.tags = ['nsfw']
-handler.command = /^(yaoi)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
