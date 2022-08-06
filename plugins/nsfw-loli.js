fef81299ecc32dcd61b7ed9d769434cc31525777
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:11:11 +0700

    Delete nsfw-loli.js

--- plugins/nsfw-loli.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/nsfw/chiisaihentai', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.nsfwloli', m)
-}
-handler.help = ['nsfwloli']
-handler.tags = ['nsfw']
-handler.command = /^(nsfwloli)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
