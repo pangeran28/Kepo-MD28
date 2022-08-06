c6ec964069f8fc0bd0238c4bfea13a31be0639b8
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:20:31 +0700

    Delete nsfw-tiddies.js

--- plugins/nsfw-tiddies.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/nsfw/biganimetiddies', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.tiddies', m)
-}
-handler.help = ['tiddies']
-handler.tags = ['nsfw']
-handler.command = /^(tiddies)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
