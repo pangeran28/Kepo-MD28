6ddf62f3a5a7e6009b0d3b00b992cd0963510063
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:19:56 +0700

    Delete nsfw-thighss.js

--- plugins/nsfw-thighss.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/nsfw/animethighss', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.thighss', m)
-}
-handler.help = ['thighss']
-handler.tags = ['nsfw']
-handler.command = /^(thighss)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
