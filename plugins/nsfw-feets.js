c3c0a6698095ba8847575b019af46d26cdf0b386
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:07:34 +0700

    Delete nsfw-feets.js

--- plugins/nsfw-feets.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/nsfw/animefeets', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.feets', m)
-}
-handler.help = ['feets']
-handler.tags = ['nsfw']
-handler.command = /^(feets)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
