e29e2502e62b69cdb17cbe20f2c2ea365c6a77ea
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:05:57 +0700

    Delete nsfw-ecchi.js

--- plugins/nsfw-ecchi.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/nsfw/ecchi', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.ecchi', m)
-}
-handler.help = ['ecchi']
-handler.tags = ['nsfw']
-handler.command = /^(ecchi)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
