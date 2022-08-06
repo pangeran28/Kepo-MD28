1f0e584acbf89145f287ca0f0b9adbf765c08341
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:22:30 +0700

    Delete nsfw-waifu.js

--- plugins/nsfw-waifu.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/nsfw/waifu', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.waifunsfw', m)
-}
-handler.help = ['waifunsfw']
-handler.tags = ['nsfw']
-handler.command = /^(waifunsfw)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
