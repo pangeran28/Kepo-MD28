bb950b486ea809777adf473ff3eee16556603af2
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:05:12 +0700

    Delete nsfw-cum.js

--- plugins/nsfw-cum.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random2/cum_jpg', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.cum', m)
-}
-handler.help = ['cum']
-handler.tags = ['nsfw']
-handler.command = /^(cum)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
