906170434e4e6ac2ed6d50918be934510d5a4be4
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:09:47 +0700

    Delete nsfw-futanari.js

--- plugins/nsfw-futanari.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random2/futanari', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.futanari', m)
-}
-handler.help = ['futanari']
-handler.tags = ['nsfw']
-handler.command = /^(futanari)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
