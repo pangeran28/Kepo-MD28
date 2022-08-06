bde5f344bf9649777a90eaa6b94ed07ec2b3323c
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:01:11 +0700

    Delete nsfw-ahegao.js

--- plugins/nsfw-ahegao.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/nsfw/ahegao', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.ahegao', m)
-}
-handler.help = ['ahegao']
-handler.tags = ['nsfw']
-handler.command = /^(ahegao)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
