0ca64e17252539f288fb2b2215a57a16f27458b1
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:18:43 +0700

    Delete nsfw-sideoppai.js

--- plugins/nsfw-sideoppai.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/nsfw/sideoppai', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.sideoppai', m)
-}
-handler.help = ['sideoppai']
-handler.tags = ['nsfw']
-handler.command = /^(sideoppai)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
