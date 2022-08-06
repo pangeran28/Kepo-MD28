1f6650926463740ebf2bf82af575f0fc40512640
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 02:38:17 +0700

    Create m-xnxx.js

---
+++ plugins/m-xnxx.js
@@ -0,0 +1,37 @@
+const uploadFile = require('../lib/uploadFile.js')
+let { webp2png } = require('../lib/webp2mp4.js')
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+	let q = m.quoted ? m.quoted : m
+	let mime = (q.msg || q).mimetype || q.mediaType || ''
+
+	if (/webp|image/g.test(mime)) {
+		let img = await q.download?.()
+		if (!img) throw `balas gambar/stiker dengan perintah ${usedPrefix + command} text\ncontoh: ${usedPrefix + command} jokowi`
+		if(!text) {
+		text = 'jokowi'
+		}
+		try {
+			let out
+			if (/image/g.test(mime)) out = await uploadFile(img)
+			if ('image/webp'.includes(mime)) out = await webp2png(img)
+			let a = (await axios.get(API('beni', 'api/canvas/xnxx', {
+				pp: out,
+				username: text
+			}), {
+				responseType: 'arraybuffer'
+			})).data
+			conn.sendFile(m.chat, a, '', 'nih bang', m)
+		} catch (e) {
+			if (e.response) {
+				log(e.response.statusText)
+				throw 'server error'
+			} else {
+				throw 'ada yang gak beres nih'
+			}
+		}
+	}
+}
+handler.help = ['xnxx']
+handler.tags = ['maker']
+handler.command = /^xnxx$/i
+module.exports = handler
