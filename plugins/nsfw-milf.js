5a77852db9417667cbba1204c9efa7e6b25f7c0c
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:11:55 +0700

    Delete nsfw-milf.js

--- plugins/nsfw-milf.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/nsfw/milf', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.milf', m)
-}
-handler.help = ['milf']
-handler.tags = ['nsfw']
-handler.command = /^(milf)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
