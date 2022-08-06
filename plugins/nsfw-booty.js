b847e9e02b7986b1daf676e3e4b9c88f257b5cf3
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 05:04:37 +0700

    Delete nsfw-booty.js

--- plugins/nsfw-booty.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/nsfw/animebooty', {}, 'apikey'), 'Nih Sayang', wm, 'Lanjut Sayang', '.booty', m)
-}
-handler.help = ['booty']
-handler.tags = ['nsfw']
-handler.command = /^(booty)$/i
-handler.premium = true
-handler.limit = true
-module.exports = handler
