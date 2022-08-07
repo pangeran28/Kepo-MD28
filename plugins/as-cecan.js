f6d849c38736d2c799758506d326da36778d4a74
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 16:30:55 +0700

    Update as-cecan.js

--- plugins/as-cecan.js
+++ plugins/as-cecan.js
@@ -1,6 +1,6 @@
 let handler = async(m,{text, conn}) => {
 let supa = 'https://api.zacros.my.id/asupan/cecan'
-conn.sendFile(m.chat, supa, null, 'Cewe cantik', m)
+conn.sendFile(m.chat, supa, null, '*Cewe Cantik Nih Om💦*\n\n_*FOLLOW IG OWNERKU OM:*_\nhttps://instagram.com/kokopangeran_', m)
 }
 handler.help = ['cecan']
 handler.tags = ['asupan']
