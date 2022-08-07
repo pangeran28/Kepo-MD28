46cf463e40b66ce17e276443f69ba99cde72727b
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 15:33:11 +0700

    Create _autoonline.js

---
+++ plugins/_autoonline.js
@@ -0,0 +1,15 @@
+let handler = async (m, { conn, usedPrefix, text, isAdmin, isOwner }) => {
+
+if (!isOwner) return m.reply('Lu Owner kah Dekkk?')
+if (autoon === true) return m.reply('Auto Online udah Aktif Banhhh,🗿🚬')
+if (autoon === true) return 
+autoon = true
+m.reply(`Success Activated Mode Auto Online`)
+ 
+}
+handler.help = ['online']
+handler.tags = ['info', 'main']
+
+handler.command = /^(online|ononline)$/i
+
+module.exports = handler
