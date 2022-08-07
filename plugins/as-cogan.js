f8bfb7bff3730424aa12fba0ec7c6273fc70e575
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 16:27:50 +0700

    Update as-cogan.js

--- plugins/as-cogan.js
+++ plugins/as-cogan.js
@@ -2,12 +2,13 @@ let scrap = require("../lib/scraper_pinterest")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	scrap.pinterest("ulzhang boy","cowok keren","cowok ganteng","cogan","cowok korea").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Done",m))
+	scrap.pinterest("ulzhang boy","cowok keren","cowok ganteng","cogan","cowok korea").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"*Nih Sayangku💦*\n\n_*FOLLOW IG OWNERKU SAYANG:*_\nhttps://instagram.com/kokopangeran_",m))
    }
     
 handler.help = ['cogan']
-handler.tags = ['internet']
+handler.tags = ['asupan']
 handler.command = /^(cogan)$/i
 handler.limit = true
+handler.premium = true
 
 module.exports = handler
