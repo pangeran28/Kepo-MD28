21455d0af0b3b8437dac2eb979c5fd560e18a9cf
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-08 01:16:38 +0700

    Update d-nhpdf.js

--- plugins/d-nhpdf.js
+++ plugins/d-nhpdf.js
@@ -1,21 +1,42 @@
-let { get } = require('axios')
-let handler = async (m, { conn, args }) => {
-	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
-  if (!args[0]) throw 'Uhm...kode nya mana?'
-  m.reply('Searching....')
-  let lol = global.API('lol', `/api/nhentaipdf/${args[0]}`, {}, 'apikey')
-  let { result } = (await get(lol)).data
-  if (result.includes('HTML')) throw `Code ${args[0]} Not Found!`
-   conn.sendMessage(m.chat, await getBuffer(result), 'documentMessage', { quoted: m, filename: `${args[0]}.pdf`, mimetype: 'application/pdf' })
+let fetch = require('node-fetch')
+let handler = async(m, { conn, usedPrefix, args, command }) => {
+  if (!args[0]) throw `Harap masukkan code sebagai parameter!\n\nContoh: ${usedPrefix + command} 304307`
+  let res1 = await fetch(global.API('lolhum', `/api/nhentai/${args[0]}`, {}, 'apikey'))
+  if (!res1.ok) throw await res1.text()
+  let json = await res1.json()
+  let ayaka = `
+Title: ${json.result.title_romanji}
+Native: ${json.result.title_native}
+Parodies: ${json.result.info.parodies}
+Tags: ${json.result.info.tags}
+Pages: ${json.result.info.pages}
+Uploaded: ${json.result.info.uploaded}
+`.trim()
+let thumbnail = await(await fetch(json.result.image[0])).buffer()
+let poi = await(await fetch(thumbfoto)).buffer()
+await conn.reply(m.chat, ayaka, m, { contextInfo: {
+  externalAdReply: {
+    mediaUrl: 'https://youtu.be/-tKVN2mAKRI',
+    title: 'Doujin Downloader',
+    body: `Code: ${args[0]}`,
+    thumbnail: thumbnail
+  }
 }
-handler.help = ['nhpdf'].map(v => v + ' <code>')
+})
+await conn.reply(m.chat, 'Uploading...', m, { contextInfo: {
+  externalAdReply: {
+    mediaUrl: 'https://youtu.be/-tKVN2mAKRI',
+    title: 'Doujin Downloader',
+    body: `Code: ${args[0]}`,
+    thumbnail: thumbnail
+  }
+}
+})
+  let res2 = await fetch(global.API('lolhum', `/api/nhentaipdf/${args[0]}`, {}, 'apikey'))
+  let hakta = await res2.json()
+  await conn.sendFile(m.chat, hakta.result, '[Haruno Bot]' + ' ' + `${args[0]}` + '.pdf', '', m, false, { asDocument: true, thumbnail: thumbnail})
+}
 handler.tags = ['downloader']
-handler.command = /^(nhpdf)$/i
-handler.limit = 1
+handler.command = /^(nh|nhentai|doujin)$/i
+handler.help = ['nhentai']
 module.exports = handler
-
-async function getBuffer(url) {
-k = await require('node-fetch')(url)
-a = await k.buffer()
-return a 
-}
