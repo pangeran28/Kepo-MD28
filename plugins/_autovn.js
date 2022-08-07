4cbd7adcb3f1100379c0ae89766f684f9dd8788e
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-07 15:36:57 +0700

    Create _autovn.js

---
+++ plugins/_autovn.js
@@ -0,0 +1,15 @@
+let handler = async (m, { conn, usedPrefix, text, isAdmin, isOwner }) => {
+
+if (!isOwner) return m.reply('Lu Owner kah Dekkk?')
+if (autovn === true) return m.reply('Auto VN udah Aktif Banh🗿🚬')
+//if (autovn === true) return 
+autovn = true
+m.reply(`Success Activated Mode Auto Recording`)
+
+}
+handler.help = ['onvn']
+handler.tags = ['info', 'main']
+
+handler.command = /^(onv|onvn)$/i
+
+module.exports = handler
