8e2a235f072ad50fec31fda5643a59eed7e00dfc
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-09 06:34:30 +0700

    Update cek.js

--- plugins/cek.js
+++ plugins/cek.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn }) => {
   m.reply('Kenapa SayangKuuuu😚', 'Apasih anj manggil owner gw mulu!', 'iya ada apa tante muach💋', 'Manggil OwnerKu Seperlunya Saja Tante😤', 'Cieee Manggil OwnerKu Terus\nSuka Ya Tante😅',)
 }
-handler.customPrefix= /^(ko?ko|pang?eran)$/i
+handler.customPrefix= /^(koko|pangeran)$/i
 handler.command = new RegExp
 
 module.exports = handler
