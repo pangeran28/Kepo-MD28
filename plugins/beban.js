38560fde7778650ca5047e10b9ccd85a70fba068
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-08 09:11:42 +0700

    Update beban.js

--- plugins/beban.js
+++ plugins/beban.js
@@ -2,7 +2,7 @@ let handler = async m => {
 
 let beban = `Sampai Jumpa Beban`
 m.reply(`
-_*Akhirnya Beban Keluarga Out Juga, Tolong Bilangin Ke Yang Tadi Keluar Nitip Kondom Buat Semua Orang Group Sini💦*_
+_*akhirnya beban Keluarga Out Juga, Tolong Bilangin Ke Yang Tadi Keluar Nitip Kondom Buat Semua Orang Group Sini💦*_
 `.trim()) // Tambah sendiri kalo mau
 }
 handler.customPrefix = /Beban/i
