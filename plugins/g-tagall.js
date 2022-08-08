1a4715c26f41b55f5158de2720696bcbfb960a12
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-09 03:56:58 +0700

    Update g-tagall.js

--- plugins/g-tagall.js
+++ plugins/g-tagall.js
@@ -3,7 +3,7 @@ let handler = async(m, { isOwner, isAdmin, conn, text, participants }) => {
                 global.dfail('admin', m, conn)
                 throw false
                 }
-  let teks = `┌─「 TAG ALL 」\n\n_*Isi Pesannya:*_ ${text ? text : ''}\n\n`
+  let teks = `┌─「 TAG ALL 」\n\n@${m.sender.split('@')[0]} : ${text ? text : ''}\n\n`
   for (let mem of participants) {
   teks += `├➥ @${mem.id.split('@')[0]}\n`}
   teks += `└────\n`
