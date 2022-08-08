ffce847100019425c661cc50b9aff3bb5ef98ff6
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-09 04:53:03 +0700

    Update fun-cantikcek.js

--- plugins/fun-cantikcek.js
+++ plugins/fun-cantikcek.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-  conn.reply(m.chat,`${pickRandom(global.cantik)}`, m)
+  conn.reply(m.chat,`${pickRandom(global.cantikk)}`, m)
 }
 handler.help = ['cantikcek']
 handler.tags = ['fun']
@@ -13,25 +13,25 @@ function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
 
-global.cantik = [
+global.cantikk = [
 'Cantik Level : 4%\n\nINI MUKA ATAU SAMPAH?!',
-'Cantik Level : 7%\n\nSerius ya Bro,, Lu ampir mirip kayak Monyet!',
+'Cantik Level : 7%\n\nSerius ya,, Lu ampir mirip kayak Monyet!',
 'Cantik Level : 12%\n\nMakin lama liat muka lo gw bisa muntah!',
-'Cantik Level : 22%\n\nMungkin karna lo sering berbuat maksiat😂',
+'Cantik Level : 22%\n\nMungkin karna lo sering liat berbuat maksiat😂',
 'Cantik Level : 27%\n\nKeknya bakal susah dapet jodoh lu,, berdoa aja',
 'Cantik Level : 35%\n\nYang sabar ya ayang',
-'Cantik Level : 41%\n\nSemoga diberkati mendapat jodohnya',
+'Cantik Level : 41%\n\nSemoga diberkati mendapat jodoh',
 'Cantik Level : 48%\n\nDijamin cowok susah deketin lo',
 'Cantik Level : 56%\n\nLu Setengah Cantik :v',
 'Cantik Level : 64%\n\nCukuplah',
-'Cantik Level : 71%\n\nLumayan Cantik Juga Lu Ya',
+'Cantik Level : 71%\n\nLumayan cantik juga lu ya',
 'Cantik Level : 2%\n\nAWOAKAK BURIQQQ!!!',
-'Cantik Level : 1%\n\nAWOAKAK BURIQQQ!!!',
-'Cantik Level : 3%\n\nAWOAKAK BURIQQQ!!!',
-'Cantik Level : 1%\n\nAWOAKAK BURIQQQ!!!',
-'Cantik Level : 77%\n\nGak akan Salah Lagi dah Tante',
-'Cantik Level : 83%\n\nDijamin Cowok gak akan Kecewa Tante',
-'Cantik Level : 89%\n\nCowok2 pasti bakalan pingsan klo ngeliat lo!',
-'Cantik Level : 94%\n\nAARRGGHHH TANTE!!!',
-'Cantik Level : 100%\n\nLU EMANG CEWE TERCANTIK YANG PERNAH GW LIAT!!!',
+'Cantik Level : 4%\n\nAWOAKAK BURIQQQ!!!',
+'Cantik Level : 1%\n\nAWOAKAK BURIQQQ SEKALI!!!',
+'Cantik Level : 6%\n\nAWOAKAK BURIQQQ!!!',
+'Cantik Level : 77%\n\nGak akan Salah Lagi dah neng',
+'Cantik Level : 83%\n\nDijamin cowok gak akan kecewa neng',
+'Cantik Level : 89%\n\ncowok2 pasti auto salfok klo ngeliat lo!',
+'Cantik Level : 94%\n\nAARRGGHHH!!!',
+'Cantik Level : 100%\n\nLU EMANG CEWEK TERCANTIK YANG PERNAH GW LIAT, GAS CHAT OWNER GUA\nwa.me/6285929970260?text=halo+sayang\nPASTI DIA LANGSUNG BAPER WKWK',
 ]
