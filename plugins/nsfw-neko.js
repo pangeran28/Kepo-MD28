98a532b6fd0d8fe2f6055d6e15d6cc1477edb4d0
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-09 05:42:56 +0700

    Update nsfw-neko.js

--- plugins/nsfw-neko.js
+++ plugins/nsfw-neko.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   if (!res.ok) throw m.reply(eror)
   let json = await res.json()
   //if (!json.url) throw m.reply(eror)
-  conn.sendBI(m.chat, `Nihh @${m.sender.split('@')[0]} jangan sagne ya!`, wm, json.url, [[`Next`, `${usedPrefix + command}`]], m, {mentions: [m.sender], jpegThumbnail: await(await fetch(json.url)).buffer() })
+  conn.sendButton(m.chat, `Nihh @${m.sender.split('@')[0]} jangan sagne ya!`, wm, json.url, [[`Next`, `${usedPrefix + command}`]], m, {mentions: [m.sender], jpegThumbnail: await(await fetch(json.url)).buffer() })
   } catch {
     //m.reply(eror)
   }
