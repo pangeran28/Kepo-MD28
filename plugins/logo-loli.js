9f09fce6677bcaa5f02ac95fef9218901a61b22c
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-10 01:06:34 +0700

    Create logo-loli.js

---
+++ plugins/logo-loli.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ').split('|')
+  if (!args[0]) throw 'Masukkan Parameter'
+  m.reply('*Wait Ngab*\nProses...')
+  let res = `https://caliphapi.com/api/lolimaker?text=${response[0]}&text2=elyas&apikey=wVLiUzSa`
+  conn.sendFile(m.chat, res, 'lolilogo.jpg', `Sudah Jadi`, m, false)
+}
+handler.help = ['logololi'].map(v => v + ' <text>')
+handler.tags = ['maker']
+handler.command = /^(logololi)$/i
+handler.limit = true
+module.exports = handler
