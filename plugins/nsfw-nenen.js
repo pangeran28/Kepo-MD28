d6826237f716b12e8171e002b203ae7eedfd7d9d
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-10 04:57:27 +0700

    Update nsfw-nenen.js

--- plugins/nsfw-nenen.js
+++ plugins/nsfw-nenen.js
@@ -3,9 +3,9 @@ if (!text) throw `Masukkan query!`
               awikwok = `NENEN NENEN KEPENGEN NENEN SAMA ${text}. TETEK GEDE NAN KENCANG MILIK ${text} MEMBUATKU KEPENGEN NENEN. DIBALUT PAKAIAN KETAT YANG ADUHAI CROOOOTOTOTOTOTOT ANJING SANGE GUA BANGSAT. ${text}, PLIS DENGERIN BAIK BAIK. TOLONG BUKA BAJU SEBENTAR SAJA PLISSS TOLOOONG BANGET, BIARKAN MULUT KERINGKU BISA MENGECAP NENEN ${text}. BIARKAN AKU MENGENYOT NENENMU ${text}. AKU RELA NGASIH SESEMBAHAN APA AJA BERAPAPUN ITU DUIT YANG AKU BAKAR KHUSUS TERKHUSUS BUATMU. TAPI TOLOOOONG BANGET BUKA BAJUMU AKU MAU NENEN. NENEN NENEEEEN NENEN ${text} WANGIIII`
               conn.reply(m.chat, awikwok, m)
 }
-handler.help = ['nenen <pasangan>']
+handler.help = ['nenen2 <pasangan>']
 handler.tags = ['nsfw']
-handler.command = /^nenen/i
+handler.command = /^nenen2/i
 handler.owner = false
 handler.mods = false
 handler.premium = true
