99ae030f7081816b5d3e2665641a7e86fef18cd6
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-09 20:16:51 +0700

    Update nsfw-loli.js

--- plugins/nsfw-loli.js
+++ plugins/nsfw-loli.js
@@ -3,7 +3,7 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'
 heum = await fetch(`https://server-api-rey.herokuapp.com/api/wallpaper/nsfwloli?apikey=apirey`)
 json = await heum.buffer()
-await conn.send2ButtonImg(m.chat, json, '*Sangne Lu Ya*', wm, 'NEXT LOLI▶️', `${usedPrefix + command}`, m, false)
+conn.sendFile(m.chat, json, '*Sangne Lu Ya*', wm, m, false)
 }
 handler.help = ['nsfwloli']
 handler.tags = ['nsfw']
