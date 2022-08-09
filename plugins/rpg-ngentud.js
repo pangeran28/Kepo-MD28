78a5ea14a52c2835c4bb1796718bf5a793bd9a25
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-10 01:23:19 +0700

    Update rpg-ngentud.js

--- plugins/rpg-ngentud.js
+++ plugins/rpg-ngentud.js
@@ -82,8 +82,9 @@ setTimeout(() => {
                      m.reply('🔎 mencari pelanggan')
                      }, 0) 
   user.lastngocok = new Date * 1
+ }
   conn.sendButton(m.chat, `Sepertinya Anda Sudah Kecapekan Silahkan Istirahat Dulu sekitar\n*🕔 ${timers}*`, wm, 'Inventory', m)
-  }
+  
 }
 handler.help = ['ngewe', 'ngentod', 'ngocok']
 handler.tags = ['rpg']
