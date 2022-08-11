e88b36d3102f3be7f29dcdd59a4a4ad75365c4c9
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-12 05:01:45 +0700

    Update d-yta.js

--- plugins/d-yta.js
+++ plugins/d-yta.js
@@ -1,5 +1,3 @@
-let axios = require('axios')
-const fetch = require('node-fetch')
 let limit = 10
 const { servers, yta } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
@@ -7,15 +5,15 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
   let chat = global.db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : servers[0])
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize 
+  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
+  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
+*Title:* ${title}
+*Filesize:* ${filesizeF}
+*${isLimit ? 'Pakai ': ''}Link:* ${await shortlink(dl_link)}
+`.trim(), m)
   if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
-┏┉━━━━━━━━━━━❏
-┆ *YOUTUBE MP3*
-├┈┈┈┈┈┈┈┈┈┈┈
-┆• *Judul:* ${title}
-│• *Type:* MP3
-┆• *📥 Ukuran File:* ${filesizeF}
-└❏
+*Title:* ${title}
+*Filesize:* ${filesizeF}
 `.trim(), m, null, {
   asDocument: chat.useDocument
 })
@@ -26,4 +24,10 @@ handler.command = /^yt(a(udio)?|mp3|musik|lagu)$/i
 handler.limit = true
 handler.register = true
 handler.premium =true
+
 module.exports = handler
+
+async function shortlink(url) {
+isurl = /https?:\/\//.test(url)
+return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
+}
