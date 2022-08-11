c01ea50641741d2f171fe99a769f2678dc71623e
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-12 03:44:30 +0700

    Update nsfw-all2.js

--- plugins/nsfw-all2.js
+++ plugins/nsfw-all2.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, command }) => {
 let nyenye = `https://api.lolhuman.xyz/api/random/nsfw/${command}?apikey=3bb99b19ba15e6a65ee4f6dd`
-    conn.sendButtonImg(m.chat, nyenye, '*Awas Sangne Ama Kartun Om:v*', wm, 'Next Sayang', `.${command}`, m) 
+    conn.sendButtonImg(m.chat, nyenye, '*Awas Sangne Sama Kartun Om:v*', wm, 'Next Sayang', `.${command}`, m) 
 }
 handler.help = ['hollolewd', 'sideoppai', 'animefeets', 'animebooty', 'animethighss', 'animearmpits', 'lewdanimegirls', 'biganimetiddies']
 handler.tags = ['nsfw']
 handler.command = /^(hollolewd|sideoppai|animefeets|animebooty|animethighss|animearmpits|lewdanimegirls|biganimetiddies)$/i
 handler.premium = true
 handler.limit = true
-handler.limit = true
+handler.register = true
 module.exports = handler
