b1348a27ea78e3badae87e8a68d085beb9565d60
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-11 07:58:41 +0700

    Update rpg-ngentud.js

--- plugins/rpg-ngentud.js
+++ plugins/rpg-ngentud.js
@@ -79,9 +79,9 @@ setTimeout(() => {
                      setTimeout(() => {
                      m.reply('🔎 mencari pelanggan')
                      }, 0) 
-  user.lastngocok = new Date * 1
-  } else conn.sendButton(m.chat, `Sepertinya Anda Sudah Kecapekan Silahkan Istirahat Dulu sekitar\n*🕔 ${timers}*`, wm, 'Profile', '.profile', m)  
-  
+ user.lastngocok = new Date * 1
+ conn.sendButton(m.chat, `Sepertinya Anda Sudah Kecapekan Silahkan Istirahat Dulu sekitar\n*🕔 ${timers}*`, wm, 'Profile', '.profile', m)  
+ }
 }
 handler.help = ['ngewe', 'ngentod', 'ngocok']
 handler.tags = ['rpg']
