956c20c11cdbe231309a65157cc96e246a150bdc
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-12 03:54:11 +0700

    Update nsfw-all.js

--- plugins/nsfw-all.js
+++ plugins/nsfw-all.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, command }) => {
 let nyenye = `https://zenzapis.xyz/api/morensfw/${command}?apikey=80b5045dce`
-    conn.sendButtonImg(m.chat, nyenye, '*Awas Sangne Sama Kartun Om:v*', wm, 'Next Sayang', `.${command}`, m) 
+    conn.sendButtonImg(m.chat, nyenye, 'Nihh @${m.sender.split('@')[0]} Jangan Sangne Om:v', wm, 'Next Sayang', `.${command}`, m) 
 }
 handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'cum', 'cuckold', 'ero', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'hentaigif', 'jahy', 'maid', 'manga', 'neko', 'orgy', 'panties', 'pussy','sfwneko', 'tentacles', 'thighs', 'yuri', 'zettairyouiki']
 handler.tags = ['nsfw']
