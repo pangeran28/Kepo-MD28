1a7e31f833086aa818e68f0c3449a4c194f64b44
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-13 03:26:47 +0700

    Create _autovn2.js

---
+++ plugins/_autovn2.js
@@ -0,0 +1,15 @@
+let handler = async (m, { conn, usedPrefix, text, isAdmin, isOwner }) => {
+
+if (!isOwner) return m.reply('Lu Owner kah Dekkk?')
+if (autovn === false) return m.reply('Auto VN udah mati Banhhh🗿🚬')
+//if (autovn === false) return 
+autovn = false
+m.reply(`Success Deactivated Mode Auto Recording`)
+
+}
+handler.help = ['offvn']
+handler.tags = ['info', 'main']
+
+handler.command = /^(offv|offvn)$/i
+
+module.exports = handler
