f0af4776f31329a4052b7676f5c7da43b046b237
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-13 03:47:18 +0700

    Update _autoall2.js

--- plugins/_autoall2.js
+++ plugins/_autoall2.js
@@ -5,13 +5,11 @@ if (!isOwner) return m.reply('Lu Owner kah Dekkk?')
 if (autoon === false) return m.reply('udah Otomatis Online bodo\n\nOwner nya bodo gesss')
 if (autoketik === false) return m.reply('udah Otomatis Ketik bodo\n\nOwner nya bodo gesss')
 if (autovn === false) return m.reply('udah Otomatis VN bodo\n\nOwner nya bodo gesss')*/
-modepublic = false
 autoreyad = false
 autoon = false
 autoketik = false
 autovn = false
 m.reply(`Success Nonaktif : 
-> Mode Public
 > Mode Auto Online
 > Mode Auto Typing
 > Mode Auto Recording
