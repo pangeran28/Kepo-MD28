9097738e01ee3078acccd5a23fb79a572b348146
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-15 10:17:39 +0700

    Update nsfw-neko.js

--- plugins/nsfw-neko.js
+++ plugins/nsfw-neko.js
@@ -1,15 +1,6 @@
 let handler = async (m, { conn, command }) => {
-  try {
-  var {age} = db.data.users[m.sender]
-  if (age <17) throw conn.reply(m.chat, 'Lu masih di bawah umur jangan dulu deh', m) 
-  let res = await fetch('https://api.waifu.pics/nsfw/neko')
-  if (!res.ok) throw m.reply(eror)
-  let json = await res.json()
-  //if (!json.url) throw m.reply(eror)
-  conn.sendButtonImg(m.chat, `Nihh @${m.sender.split('@')[0]} jangan sagne ya!`, wm, json.url, `Next`, `.${command}`, m, {mentions: [m.sender], jpegThumbnail: await(await fetch(json.url)).buffer() })
-  } catch {
-    //m.reply(eror)
-  }
+let nyenye = `https://api.waifu.pics/nsfw/neko`
+    conn.sendButtonImg(m.chat, nyenye, `Nihh @${m.sender.split('@')[0]} Jangan Sagne Om!`, wm, `Next Sayang`, `.${command}`, m) 
 }
 handler.help = ['nsfwneko']
 handler.tags = ['nsfw']
