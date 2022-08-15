df73c04de84d9baa3cdd8813d4c7b45017848072
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-15 10:10:39 +0700

    Update nsfw-all3.js

--- plugins/nsfw-all3.js
+++ plugins/nsfw-all3.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, command }) => {
-let nyenye = `https://api.lolhuman.xyz/api/random2/${command}?apikey=3bb99b19ba15e6a65ee4f6dd}`
+let nyenye = `https://api.lolhuman.xyz/api/random2/${command}?apikey=sonelganz}`
     conn.sendButtonImg(m.chat, nyenye, `Nihh @${m.sender.split('@')[0]} Jangan Sagne Om!`, wm, `Next Sayang`, `.${command}`, m) 
 }
 handler.help = ['lewd', 'eron', 'solo', 'anal', 'keta', 'tits', 'kuni', 'solog', 'erok', 'feetg', 'lewdk', 'erofeet', 'holoero', 'classic', 'erokemo', 'futanari', 'eroyuri', 'yaoi']
