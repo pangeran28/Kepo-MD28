f9a6b06aafe427b509594611720bc02a10e689d2
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-15 10:15:24 +0700

    Update nsfw-loli.js

--- plugins/nsfw-loli.js
+++ plugins/nsfw-loli.js
@@ -1,15 +1,6 @@
 let handler = async (m, { conn, command }) => {
-  try {
-  var {age} = db.data.users[m.sender]
-  if (age <17) throw conn.reply(m.chat, 'Lu masih di bawah umur jangan dulu deh', m) 
-  let res = await fetch('https://api.lolhuman.xyz/api/random/nsfw/loli?apikey=eafcf901640d1c599ae71519')
-  if (!res.ok) throw m.reply(eror)
-  let json = await res.json()
-  // (!json.url) throw m.reply(eror)
-  conn.sendButtonImg(m.chat, `Nihh @${m.sender.split('@')[0]} jangan sagne ya!`, wm, json.url, `Next`, `.${command}`, m, {mentions: [m.sender], jpegThumbnail: await(await fetch(json.url)).buffer() })
-  } catch {
-    //m.reply(eror)
-  }
+let nyenye = `https://api.lolhuman.xyz/api/random/nsfw/loli?apikey=eafcf901640d1c599ae71519`
+    conn.sendButtonImg(m.chat, nyenye, `Nihh @${m.sender.split('@')[0]} Jangan Sagne Om!`, wm, `Next Sayang`, `.${command}`, m) 
 }
 handler.help = ['nsfwloli']
 handler.tags = ['nsfw']
