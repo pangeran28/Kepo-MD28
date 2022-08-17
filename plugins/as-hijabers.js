1a856d95224199227a152a19d48e78315445be4d
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-17 17:42:07 +0700

    Update as-hijabers.js

--- plugins/as-hijabers.js
+++ plugins/as-hijabers.js
@@ -1,10 +1,11 @@
-let handler = async(m,{text, conn}) => {
+let handler = async (m, { conn, command }) => {
 let supa = 'https://api.zacros.my.id/asupan/hijaber'
-conn.sendFile(m.chat, supa, null, 'hijaber', m)
+conn.sendButtonImg(m.chat, supa, `Nihh @${m.sender.split('@')[0]} Cewe Hijabers Om!`, wm, `Next Sayang`, `.${command}`, m)
 }
-handler.help = ['tudung']
+handler.help = ['hijab']
 handler.tags = ['asupan']
-handler.command = /^(tudung)$/i
+handler.command = /^(hijab)$/i
 handler.premium = true
 handler.limit = true
+handler.register = true
 module.exports = handler
