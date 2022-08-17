4ba28bd06667d9291cdb4ba005f322beee31058a
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-18 02:07:11 +0700

    Update as-jepang.js

--- plugins/as-jepang.js
+++ plugins/as-jepang.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, command }) => {
+/*let handler = async (m, { conn, command }) => {
 let supa = 'https://api.zacros.my.id/asupan/japan'
 conn.sendButtonImg(m.chat, supa, `Nihh @${m.sender.split('@')[0]} Cewe Jepangnya Om!`, wm, `Next Sayang`, `.${command}`, m)
 }
@@ -8,4 +8,4 @@ handler.command = /^(jepang)$/i
 handler.premium = true
 handler.limit = true
 handler.register = true
-module.exports = handler
+module.exports = handler*/
