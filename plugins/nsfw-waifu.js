0e09a969df3cc63246914fcdabcd1fa396adbb17
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-17 18:11:44 +0700

    Update nsfw-waifu.js

--- plugins/nsfw-waifu.js
+++ plugins/nsfw-waifu.js
@@ -1,8 +1,4 @@
 let handler = async (m, { conn, command }) => {
-let nyenye = `https://api.waifu.pics/nsfw/waifu`
-    conn.sendButtonImg(m.chat, nyenye, `Nihh @${m.sender.split('@')[0]} Jangan Sagne Om!`, wm, `Next Sayang`, `.${command}`, m) 
-}
-/*let handler = async (m, { conn, command }) => {
   try {
   var {age} = db.data.users[m.sender]
   if (age <17) throw conn.reply(m.chat, 'Lu masih di bawah umur jangan dulu deh', m)
@@ -10,11 +6,11 @@ let nyenye = `https://api.waifu.pics/nsfw/waifu`
   //if (!res.ok) throw `sorry banh gk nemu, coba lagi<3`
   let json = await res.json()
   if (!json.url) throw m.reply(eror)
-  conn.sendButtonImg(m.chat, `Nihh @${m.sender.split('@')[0]} jangan sagne ya!`, wm, json.url, `Next`, `.${command}`, m, {mentions: [m.sender], jpegThumbnail: await(await fetch(json.url)).buffer()})
+  conn.sendButton(m.chat, `Nihh @${m.sender.split('@')[0]} jangan sagne ya!`, wm, json.url, `Next`, `.${command}`, m, {mentions: [m.sender], jpegThumbnail: await(await fetch(json.url)).buffer()})
   } catch {
   
   }
-}*/
+}
 handler.help = ['nsfwwaifu']
 handler.tags = ['nsfw']
 handler.command = /^(nsfwwaifu|waifunsfw)$/i
