6ce58754ecfdaa9d3c9555593757fed66e7ca1bd
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-18 05:26:26 +0700

    Update quotesanime.js

--- plugins/quotesanime.js
+++ plugins/quotesanime.js
@@ -1,4 +1,4 @@
-let axios = require("axios");
+/*let axios = require("axios");
 let handler = async(m, { conn, text }) => {
 
 	axios.get(`https://videfikri.com/api/anime/randomquoteanime`).then ((res) => {
@@ -22,5 +22,20 @@ handler.botAdmin = false
 handler.fail = null
 handler.exp = 0
 handler.limit = false
+
+module.exports = handler*/
+
+
+let fetch = require('node-fetch')
+let fs = require('fs')
+let handler = async(m, { conn, usedPrefix, text, command }) => {
+  let json = await require("../lib/scrape").quotesAnime('')
+  conn.sendButtonImg(m.chat, json.gambar, `${json.quotes} - By _${json.karakter}_ \nEpisode : ${json.episode}\nFrom Anime : ${json.anime}\nLink : ${json.link}`, `${json.up_at}`, `Lainnya`, `${usedPrefix + command}`, ftoko)
+}
+handler.help = ['quotesanim']
+handler.tags = ['anime']
+handler.command = /^(quotesanim)$/i
+handler.register = true
+handler.limit = true
 
 module.exports = handler
