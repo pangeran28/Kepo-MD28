0ca098284fc39c9336f69fd65b3266d0bf6108f1
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-18 02:08:14 +0700

    Update as-korea.js

--- plugins/as-korea.js
+++ plugins/as-korea.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, command }) => {
+/*let handler = async (m, { conn, command }) => {
 let supa = 'https://api.zacros.my.id/asupan/korea'
 conn.sendButtonImg(m.chat, supa, `Nihh @${m.sender.split('@')[0]} Cewe Koreanya Om!`, wm, `Next Sayang`, `.${command}`, m)
 }
@@ -8,4 +8,4 @@ handler.command = /^(korea)$/i
 handler.premium = true
 handler.register = true
 handler.limit = true
-module.exports = handler
+module.exports = handler*/
