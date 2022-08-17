6035520799d77a9ae9a893d43a80466ac9731b5b
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-18 02:05:56 +0700

    Update as-indonesia.js

--- plugins/as-indonesia.js
+++ plugins/as-indonesia.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, command }) => {
+/*let handler = async (m, { conn, command }) => {
 let supa = 'https://api.zacros.my.id/asupan/Indonesia'
 conn.sendButtonImg(m.chat, supa, `Nihh @${m.sender.split('@')[0]} Cewe Indonya Om!`, wm, `Next Sayang`, `.${command}`, m)
 }
@@ -8,4 +8,4 @@ handler.command = /^(indo)$/i
 handler.premium = true
 handler.limit = true
 handler.register = true
-module.exports = handler
+module.exports = handler*/
