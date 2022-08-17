177af9103346cc38d7cdcf5dac8669c2d2d5d21a
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-17 18:24:02 +0700

    Update as-santuy.js

--- plugins/as-santuy.js
+++ plugins/as-santuy.js
@@ -1,10 +1,11 @@
-let handler = async(m,{text, conn}) => {
+let handler = async (m, { conn, command }) => {
 let supa = 'https://api.zacros.my.id/asupan/santuy'
-conn.sendFile(m.chat, supa, null, 'Nih Om💦\n\n_*Follow IG OwnerKu:*_\nhttps://instagram.com/kokopangeran_', m)
+conn.sendButtonImg(m.chat, supa, `Nihh @${m.sender.split('@')[0]} Cewe Santuy Om!`, wm, `Next Sayang`, `.${command}`, m)
 }
 handler.help = ['santuy']
 handler.tags = ['asupan']
 handler.command = /^(santuy)$/i
 handler.premium = true
 handler.limit = true
+handler.register = true
 module.exports = handler
