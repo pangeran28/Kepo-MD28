1ff92ac81a73972e8d9dee49f87d3ebb0c1ec9fd
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-17 17:55:17 +0700

    Update and rename as-thailand to as-thailand.js

---
+++ plugins/as-thailand.js
@@ -0,0 +1,11 @@
+let handler = async (m, { conn, command }) => {
+let supa = 'https://api.zacros.my.id/asupan/thailand'
+conn.sendButtonImg(m.chat, supa, `Nihh @${m.sender.split('@')[0]} Cewe Thailandnya Om!`, wm, `Next Sayang`, `.${command}`, m)
+}
+handler.help = ['thailand']
+handler.tags = ['asupan']
+handler.command = /^(thailand)$/i
+handler.premium = true
+handler.limit true
+handler.register = true
+module.exports = handler
