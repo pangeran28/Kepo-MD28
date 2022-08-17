5f6e03840316ed41298b093557e9fa95677acdbd
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-17 19:00:54 +0700

    Update as-cecan.js

--- plugins/as-cecan.js
+++ plugins/as-cecan.js
@@ -1,7 +1,14 @@
-let handler = async (m, { conn, command }) => {
+/*let handler = async (m, { conn, command }) => {
 let supa = 'https://api.zacros.my.id/asupan/cecan'
 conn.sendButtonImg(m.chat, supa, `Nihh @${m.sender.split('@')[0]} Cewe Cantiknya Om!`, wm, `Next Sayang`, `.${command}`, m)
-}
+}\*
+let scrap = require("../lib/scraper_pinterest3")
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+	scrap.pinterest("ulzhang girl","cewek keren","cewek cantik","cecan","cewek korea","cewek hijab","cewek thailand","cewek malaysia","cewek jepang","blackpink","cewek vietnam","cewek gemoy").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"*Nih Om💦*\n\n_*FOLLOW IG OWNERKU OM:*_\nhttps://instagram.com/kokopangeran_",m))
+   }
+
 handler.help = ['cecan']
 handler.tags = ['asupan']
 handler.command = /^(cecan)$/i
