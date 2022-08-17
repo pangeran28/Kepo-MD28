be0a3f2319151c44791cfe3a9f8791e4df951dbc
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-17 17:59:29 +0700

    Update as-ukhty.js

--- plugins/as-ukhty.js
+++ plugins/as-ukhty.js
@@ -1,10 +1,11 @@
-let handler = async(m,{text, conn}) => {
+let handler = async (m, { conn, command }) => {
 let supa = 'https://api.zacros.my.id/asupan/ukhty'
-conn.sendFile(m.chat, supa, null, 'Nih', m)
+conn.sendButtonImg(m.chat, supa, `Nihh @${m.sender.split('@')[0]} Cewe Hijabers Kedua Om!`, wm, `Next Sayang`, `.${command}`, m)
 }
 handler.help = ['ukhty']
 handler.tags = ['asupan']
 handler.command = /^(ukhty)$/i
 handler.premium = true
 handler.limit = true
+handler.register = true
 module.exports = handler
