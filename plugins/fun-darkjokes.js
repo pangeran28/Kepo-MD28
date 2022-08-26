dc6701098580a6f992b5b0be6c37977ba888b869
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-26 16:05:26 +0700

    Update fun-darkjokes.js

--- plugins/fun-darkjokes.js
+++ plugins/fun-darkjokes.js
@@ -1,4 +1,27 @@
 let handler = async (m, { conn }) => {
+    conn.sendButtonImg(m.chat, global.API('lol', '/api/meme/darkjoke', {}, 'apikey'), 'Dark Jokes', wm, 'Next!', '.darkjokes', m, {
+  contextInfo: { mentionedJid: [m.sender],
+    externalAdReply :{
+    showAdAttribution: true,
+    mediaUrl: data.sc,
+    mediaType: 2,
+    description: data.deslink , 
+    title: run,
+    body: wm, 
+    thumbnail: bg,
+    sourceUrl: data.sc
+     }}
+  })
+}
+handler.help = ['darkjokes']
+handler.tags = ['internet']
+handler.command = /^(dragjokes|darkjokes)$/i
+handler.limit = true
+handler.register = true
+module.exports = handler
+
+
+/*let handler = async (m, { conn }) => {
 let rest = 'https://api.zacros.my.id/randomimg/darkjokes'
     conn.sendButtonImg(m.chat, rest, 'Dark? 🤨', wm, 'NEXT', '.darkjokes', m)
 }
@@ -6,4 +29,4 @@ handler.help = ['darkjokes']
 handler.tags = ['internet', 'fun']
 handler.command = /^(dragjokes|darkjokes)$/i
 
-module.exports = handler
+module.exports = handler*/
