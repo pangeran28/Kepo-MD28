252879ef4f272c012672ac8cb8fb2cd3a2e2c4c1
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-26 16:33:23 +0700

    Update d-nhpdf.js

--- plugins/d-nhpdf.js
+++ plugins/d-nhpdf.js
@@ -1,4 +1,4 @@
-let { get } = require('axios')
+/*let { get } = require('axios')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Uhm...kode nya mana?'
@@ -23,6 +23,30 @@ handler.command = /^(nhpdf|nh)$/i
 handler.limit = true
 handler.premium = true
 handler.register = true
+module.exports = handler
+
+async function getBuffer(url) {
+k = await require('node-fetch')(url)
+a = await k.buffer()
+return a 
+}*/
+
+let { get } = require('axios')
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...kode nya mana?'
+  m.reply('Searching....')
+  let lol = global.API('lol', `/api/nhentaipdf/${args[0]}`, {}, 'apikey')
+  let { result } = (await get(lol)).data
+  if (result.includes('HTML')) throw `Code ${args[0]} Not Found!`
+   conn.sendMessage(m.chat, await getBuffer(result), 'documentMessage', { quoted: m, filename: `${args[0]}.pdf`, mimetype: 'application/pdf' })
+}
+handler.help = ['nhpdf'].map(v => v + ' <code>')
+handler.tags = ['nsfw']
+handler.command = /^(nhpdf)$/i
+handler.limit = true
+handler.premium = true
+handler.register = true
+handler.nsfw = true
 module.exports = handler
 
 async function getBuffer(url) {
