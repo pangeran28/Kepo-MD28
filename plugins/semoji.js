d212a71476bafefdedf01441bd95e22302024965
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-26 17:08:02 +0700

    Update semoji.js

--- plugins/semoji.js
+++ plugins/semoji.js
@@ -1,36 +1,61 @@
-const { MessageType } = require('@adiwajshing/baileys')
-const { sticker } = require('../lib/sticker')
-const fetch = require('node-fetch')
 const { EmojiAPI } = require("emoji-api")
 const emoji = new EmojiAPI()
+let handler = async(m, { conn, usedPrefix, command, args, text, }) => {
+    if(!text) throw `Example : *${usedPrefix + command} 😳*`
+    let ras = `Halo Kak @${m.sender.split('@')[0]}`
+    let sel = `Emojinya Kan Ada Beberapa Jenis Tuh, Kakak Mau Emoji Jenis Apa? Pilih Dibawah Ya Kak!\nHarap Perhatikan Cuma Bisa 1 Emoji Dan Gunakan Emoji!`
+    let rs = `Pilih Disini`
+const sections = [ {
+	title: `Silahkan Pilih List Type Emoji Dibawah!`,
+	rows: [
+	{title: `Emoji ${args[0]} Apple`, rowId: `#emoji apple ${args[0]}`, description: ``},
+        {title: `Emoji ${args[0]} Facebook`, rowId: `#emoji facebook ${args[0]}`, description: ``},
+	{title: `Emoji ${args[0]} Google`, rowId: `#emoji google ${args[0]}`, description: ``},
+	{title: `Emoji ${args[0]} Htc`, rowId: `#emoji htc ${args[0]}`, description: ``},
+	{title: `Emoji ${args[0]} Instagram`, rowId: `#emoji instagram ${args[0]}`, description: ``},
+	{title: `Emoji ${args[0]} Microsoft`, rowId: `#emoji microsoft ${args[0]}`, description: ``},
+	{title: `Emoji ${args[0]} Mozilla`, rowId: `#emoji mozilla ${args[0]}`, description: ``},
+	{title: `Emoji ${args[0]} Openmoji`, rowId: `#emoji openmoji ${args[0]}`, description: ``},
+	{title: `Emoji ${args[0]} Pixel`, rowId: `#emoji pixel ${args[0]}`, description: ``},
+	{title: `Emoji ${args[0]} Samsung`, rowId: `#emoji samsung ${args[0]}`, description: ``},
+	{title: `Emoji ${args[0]} Twitter`, rowId: `#emoji twitter ${args[0]}`, description: ``},
+	{title: `Emoji ${args[0]} WhatsApp`, rowId: `#emoji whatsapp ${args[0]}`, description: ``},
+               ]
+    } ]
 
-let handler = async (m, { conn, args, usedPrefix, command, isPrems }) => {
-  let er = `contoh:
-${usedPrefix}semoji whatsapp ðŸ˜Ž
+const listMessage = {
+  text: sel,
+  mentions: [m.sender],
+  footer: wm,
+  title: ras,
+  buttonText: rs,
+  sections
+}
 
-*Opsi*
-- apple
-- facebook
-- google
-- htc
-- lg
-- microsoft
-- mozilla
-- openmoji
-- pixel
-- samsung
-- twitter
-- whatsapp
-- messenger
-- emojidex
-- softbank
-- docomo
-- au_by_kddi
-`.trim()
-  if (!args[0]) throw er
+    if(!args[1]) await conn.sendMessage(m.chat, listMessage, { quoted: m })
 
+    let er = `contoh:
+*${usedPrefix}emo(spasi)ap(spasi)❤️*
+*${usedPrefix}emo ap ❤️*
+${data.sa}${data.kki} ${data.zt}Opsi${data.zt} ${data.kka}
+${data.gy} ap = apple
+${data.gy} fa = facebook
+${data.gy} go = google
+${data.gy} ht = htc
+${data.gy} ig = instagram 
+${data.gy} mi = microsoft
+${data.gy} mo = mozilla
+${data.gy} op = openmoji
+${data.gy} pi = pixel
+${data.gy} sa = samsung
+${data.gy} tw = twitter
+${data.gy} wh = whatsapp
+${data.sb}
+Hanya bisa 1 emoji, perhatikan spasi, jangan spam!
+`
+  if(!args[0]) throw er
+  //if (!args[1]) throw er
   let template = (args[0] || '').toLowerCase()
-  if (!args[1]) throw er
   if (/emo/i.test(command)) {
     try {
       switch (template) {
@@ -39,10 +64,7 @@ ${usedPrefix}semoji whatsapp ðŸ˜Ž
         case 'ap':
           emoji.get(`${args[1]}`)
             .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[0].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
+              await conn.sendStimg(m.chat, emoji.images[0].url, m, { packname: packname, author: author })
             })
           break
         case 'facebook':
@@ -50,39 +72,28 @@ ${usedPrefix}semoji whatsapp ðŸ˜Ž
         case 'fa':
           emoji.get(`${args[1]}`)
             .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[6].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
+              await conn.sendStimg(m.chat, emoji.images[6].url, m, { packname: packname, author: author })
             })
           break
         case 'google':
         case 'go':
           emoji.get(`${args[1]}`)
             .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[1].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
+              await conn.sendStimg(m.chat, emoji.images[1].url, m, { packname: packname, author: author })
             })
           break
         case 'htc':
         case 'ht':
           emoji.get(`${args[1]}`)
             .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[12].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
+              await conn.sendStimg(m.chat, emoji.images[12].url, m, { packname: packname, author: author })
             })
-          break 
-        case 'lg':
+          break
+        case 'Ig':
+        case 'instagram':
           emoji.get(`${args[1]}`)
             .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[11].url, global.packname, global.author)
-             conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
+              await conn.sendStimg(m.chat, emoji.images[11].url, m, { packname: packname, author: author })
             })
           break
         case 'microsoft':
@@ -90,10 +101,7 @@ ${usedPrefix}semoji whatsapp ðŸ˜Ž
         case 'mi':
           emoji.get(`${args[1]}`)
             .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[3].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
+              await conn.sendStimg(m.chat, emoji.images[3].url, m, { packname: packname, author: author })
             })
           break
         case 'mozilla':
@@ -101,10 +109,7 @@ ${usedPrefix}semoji whatsapp ðŸ˜Ž
         case 'mo':
           emoji.get(`${args[1]}`)
             .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[13].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
+              await conn.sendStimg(m.chat, emoji.images[13].url, m, { packname: packname, author: author })
             })
           break
         case 'openmoji':
@@ -112,20 +117,14 @@ ${usedPrefix}semoji whatsapp ðŸ˜Ž
         case 'op':
           emoji.get(`${args[1]}`)
             .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[8].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
+              await conn.sendStimg(m.chat, emoji.images[8].url, m, { packname: packname, author: author })
             })
           break
-        case 'joypixels':
+        case 'pixel':
         case 'pi':
           emoji.get(`${args[1]}`)
             .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[7].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
+              await conn.sendStimg(m.chat, emoji.images[7].url, m, { packname: packname, author: author })
             })
           break
         case 'samsung':
@@ -133,20 +132,14 @@ ${usedPrefix}semoji whatsapp ðŸ˜Ž
         case 'sa':
           emoji.get(`${args[1]}`)
             .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[2].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
+              await conn.sendStimg(m.chat, emoji.images[2].url, m, { packname: packname, author: author })
             })
           break
         case 'twitter':
         case 'tw':
           emoji.get(`${args[1]}`)
             .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[5].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
+              await conn.sendStimg(m.chat, emoji.images[5].url, m, { packname: packname, author: author })
             })
           break
         case 'whatsapp':
@@ -154,56 +147,7 @@ ${usedPrefix}semoji whatsapp ðŸ˜Ž
         case 'wh':
           emoji.get(`${args[1]}`)
             .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[4].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
-            })
-          break
-        case 'softbank':
-        case 'sb':
-          emoji.get(`${args[1]}`)
-            .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[14].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
-            })
-          break
-        case 'docomo':
-          emoji.get(`${args[1]}`)
-            .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[15].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
-            })
-          break
-        case 'messenger':
-          emoji.get(`${args[1]}`)
-            .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[10].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
-            })
-          break
-        case 'emojidex':
-          emoji.get(`${args[1]}`)
-            .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[9].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
-            })
-          break
-        case 'au_by_kddi':
-          emoji.get(`${args[1]}`)
-            .then(async emoji => {
-              let stiker = await sticker(false, emoji.images[16].url, global.packname, global.author)
-              conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, {
-                asSticker: true
-                })
+              await conn.sendStimg(m.chat, emoji.images[4].url, m, { packname: packname, author: author })
             })
           break
       }
@@ -212,9 +156,9 @@ ${usedPrefix}semoji whatsapp ðŸ˜Ž
     }
   }
 }
-handler.help = ['semoji']
+handler.help = ['semoji'].map(v => v + ' <emoji>')
 handler.tags = ['sticker']
 handler.command = /^((s(tic?ker)?)?emo(ji)?)$/i
 handler.limit = true
-
+handler.register = true
 module.exports = handler
