49a672d18320133887f2619ed233c2ce34370a5f
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-26 19:28:09 +0700

    Update o-setbotpp.js

--- plugins/o-setbotpp.js
+++ plugins/o-setbotpp.js
@@ -1,4 +1,38 @@
+const fs = require('fs')
 let handler = async (m, { conn, isROwner }) => {
+ if (!isROwner) throw false
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+var media = await q.download()
+            let botNumber = await conn.user.jid
+            var { img } = await conn.generateProfilePicture(media)
+            await conn.query({
+            tag: 'iq',
+            attrs: {
+            to: botNumber,
+            type:'set',
+            xmlns: 'w:profile:picture'
+            },
+            content: [
+            {
+            tag: 'picture',
+            attrs: { type: 'image' },
+            content: img
+            }
+            ]
+            })
+            m.reply(`Sukses`)
+}
+handler.help = ['setbotpp']
+handler.tags = ['owner']
+handler.command = /^(set(bot)?pp)$/i
+
+handler.owner = true
+
+module.exports = handler
+
+
+/*let handler = async (m, { conn, isROwner }) => {
     if (!isROwner) throw false
     let bot = conn.user.jid // Bot
     let q = m.quoted ? m.quoted : m
@@ -14,4 +48,4 @@ handler.help = ['setbotpp']
 handler.tags = ['owner']
 handler.command = /^(set(bot)?pp)$/i
 
-module.exports = handler
+module.exports = handler*/
