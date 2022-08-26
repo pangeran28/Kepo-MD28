2f599dcf27ef6455b332ca4d7436f8835c07a3a5
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-26 21:53:34 +0700

    Update fun-ceritahorror.js

--- plugins/fun-ceritahorror.js
+++ plugins/fun-ceritahorror.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
 let res = await fetch('https://api.lolhuman.xyz/api/ceritahoror?apikey=SGWN')
     let json = await res.json()
     
-    await m.reply(data.wait)
+    await m.reply(global.wait)
 await conn.sendButtonImg(m.chat, json.result.thumbnail, `Judul: ${json.result.title}
 Desc: ${json.result.desc}
 Cerita: ${json.result.story}`, wm, 'Again', '.ceritahoror', m)
