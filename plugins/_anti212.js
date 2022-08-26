cf5d9e378ba8e0366f23c0220d685489e0a80335
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-26 15:25:35 +0700

    Update _anti212.js

--- plugins/_anti212.js
+++ plugins/_anti212.js
@@ -1,5 +1,51 @@
 let handler = m => m
 
+handler.before = async function (m) {
+   if (m.sender.startsWith('212' || '212')) {
+   	global.db.data.users[m.sender].banned = true
+conn.reply('Awkawk Orang Asin :l')
+this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+this.updateBlockStatus(m.sender, 'block')
+   }
+   
+   if (m.sender.startsWith('92' || '92')) {
+   	global.db.data.users[m.sender].banned = true
+conn.reply('Awkawk Orang Asin :l')
+this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+this.updateBlockStatus(m.sender, 'block')
+   }
+   if (m.sender.startsWith('1' || '1')) {
+   	global.db.data.users[m.sender].banned = true
+conn.reply('Awkawk Orang Asin :l')
+this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+this.updateBlockStatus(m.sender, 'block')
+   }
+   if (m.sender.startsWith('91' || '91')) {
+   	global.db.data.users[m.sender].banned = true
+conn.reply('Awkawk Orang Asin :l')
+this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+this.updateBlockStatus(m.sender, 'block')
+   }
+   if (m.sender.startsWith('90' || '90')) {
+   	global.db.data.users[m.sender].banned = true
+conn.reply('Awkawk Orang Asin :l')
+this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+this.updateBlockStatus(m.sender, 'block')
+   }
+   if (m.sender.startsWith('7' || '7')) {
+   	global.db.data.users[m.sender].banned = true
+conn.reply('Awkawk Orang Asin :l')
+this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+this.updateBlockStatus(m.sender, 'block')
+   }
+    }
+
+module.exports = handler
+
+
+
+/*let handler = m => m
+
 export async function before(m, { isAdmin, isBotAdmin }) {
    if (m.sender.startsWith('212' || '212')) {
    	global.db.data.users[m.sender].banned = true
@@ -28,4 +74,4 @@ conn.groupParticipantsUpdate(m.chat, [user], "remove")
 return true
 }
 
-module.exports = handler
+module.exports = handler*/
